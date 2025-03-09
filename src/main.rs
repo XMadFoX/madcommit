2b0abc5474d8e746e6fe6ece9c388102d7c4d15c
MadFox <madfox@laserflare.net>
2025-03-09 20:03:41 +0400

    feat(gen): add genai to generate commit messages

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+use std::fs;
+
 use git2::{DiffOptions, Error, Repository};
 
+use genai::chat::printer::{print_chat_stream, PrintChatStreamOptions};
+use genai::chat::{ChatMessage, ChatRequest};
+use genai::Client;
 use simple_logger::SimpleLogger;
+
+const MODEL: &str = "gpt-4o-mini";
+
 fn get_pretty_diff(repo: &Repository, context_lines: u32) -> Result<String, Error> {
     let mut output = String::new();
 
@@ -58,13 +66,36 @@ fn get_pretty_diff(repo: &Repository, context_lines: u32) -> Result<String, Erro
     Ok(output)
 }
 
-fn main() -> Result<(), Error> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     SimpleLogger::new().env().init().unwrap();
+
     let repo = Repository::open("./")?;
 
     let diff_string = get_pretty_diff(&repo, 3)?;
 
-    println!("{}", diff_string);
+    // read template.md
+    let template = fs::read_to_string("template.md")?;
+
+    let chat_req = ChatRequest::new(vec![
+        ChatMessage::system(template),
+        ChatMessage::user(&diff_string),
+    ]);
+
+    let client = Client::default();
+
+    let print_options = PrintChatStreamOptions::from_print_events(false);
+
+    let adapter_kind = client.resolve_service_target(MODEL)?.model.adapter_kind;
+
+    log::debug!("Using {MODEL} ({adapter_kind})");
+
+    log::debug!("Answer: (streaming)");
+    let chat_res = client
+        .exec_chat_stream(MODEL, chat_req.clone(), None)
+        .await?;
+    let commit_msg = print_chat_stream(chat_res, Some(&print_options)).await?;
+    log::debug!("Result:\n{commit_msg}");
 
     Ok(())
 }
