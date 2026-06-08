297e4308fb3b1f2a1d655608d01359c2818703fc
MadFox <madfox@laserflare.net>
2026-06-08 23:06:46 +0400

    build: add nix package build in flake

--- flake.nix
+++ flake.nix
@@ -5,38 +5,101 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { nixpkgs, ... }: let
-    system = builtins.currentSystem;
-
-    in {
-      devShells."${system}".default = let
-        pkgs = import nixpkgs {
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+      pkgsFor =
+        system:
+        import nixpkgs {
           inherit system;
         };
 
-      in pkgs.mkShell {
-        packages = with pkgs; [
-          cargo
-          rustc
-          clippy
-          rust-analyzer
-          rustfmt
-          git
-          toybox
-          openssl
-          pkg-config
-          gcc
-        ];
+      packageFor =
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        pkgs.rustPlatform.buildRustPackage {
+          pname = "madcommit";
+          version = "0.1.0";
 
-        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-        RUSTFLAGS = "-C link-arg=-Wl,-rpath=${pkgs.lib.makeLibraryPath [ pkgs.openssl ]}";
-        NIX_LDFLAGS = "-rpath ${pkgs.lib.makeLibraryPath [ pkgs.openssl ]}";
-        CC = "${pkgs.gcc}/bin/gcc";
-        CXX = "${pkgs.gcc}/bin/g++";
-        OPENSSL_DIR = "${pkgs.openssl.dev}";
-        OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
-        OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
-        OPENSSL_NO_VENDOR = "1";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+          buildInputs = with pkgs; [ openssl ];
+
+          postPatch = ''
+            substituteInPlace src/config.rs \
+              --replace-fail 'template_path: "template.md".to_string()' \
+                             'template_path: "'"$out"'/share/madcommit/template.md".to_string()'
+          '';
+
+          postInstall = ''
+            install -Dm644 template.md $out/share/madcommit/template.md
+          '';
+
+          OPENSSL_NO_VENDOR = "1";
         };
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          madcommit = packageFor system;
+        in
+        {
+          inherit madcommit;
+          default = madcommit;
+        }
+      );
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${packageFor system}/bin/madcommit";
+        };
+      });
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+          libPath = pkgs.lib.makeLibraryPath [ pkgs.openssl ];
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              cargo
+              rustc
+              clippy
+              rust-analyzer
+              rustfmt
+              git
+              toybox
+              openssl
+              pkg-config
+              gcc
+            ];
+
+            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+            RUSTFLAGS = "-C link-arg=-Wl,-rpath=${libPath}";
+            NIX_LDFLAGS = "-rpath ${libPath}";
+            CC = "${pkgs.gcc}/bin/gcc";
+            CXX = "${pkgs.gcc}/bin/g++";
+            OPENSSL_DIR = "${pkgs.openssl.dev}";
+            OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+            OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
+            OPENSSL_NO_VENDOR = "1";
+          };
+        }
+      );
     };
 }
