513855fb7b5c7c292f1ead7decb757de490e94c6
MadFox <madfox@laserflare.net>
2026-06-08 23:03:53 +0400

    fix(env): add openssl, gcc and set paths for correct building & running

--- flake.nix
+++ flake.nix
@@ -25,9 +25,18 @@
           toybox
           openssl
           pkg-config
+          gcc
         ];
 
         RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        RUSTFLAGS = "-C link-arg=-Wl,-rpath=${pkgs.lib.makeLibraryPath [ pkgs.openssl ]}";
+        NIX_LDFLAGS = "-rpath ${pkgs.lib.makeLibraryPath [ pkgs.openssl ]}";
+        CC = "${pkgs.gcc}/bin/gcc";
+        CXX = "${pkgs.gcc}/bin/g++";
+        OPENSSL_DIR = "${pkgs.openssl.dev}";
+        OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+        OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
+        OPENSSL_NO_VENDOR = "1";
         };
     };
 }
